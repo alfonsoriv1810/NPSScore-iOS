d1f76a62da6875e9850ec23600bd6083c3a804a7
Alfonso Rivera Cortes <alfonso.riv1810@gmail.com>
2019-02-08 23:07:39 -0600

    8-Feb-2019 Corrección de estructura de git
    -Se corrige la estructura del git para tener mejor orden del proyecto.

---
+++ NPSScore/Models/KBaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  KBaseModel.swift
+//  NPSScore
+//
+//  Created by Alfonso Rivera Cortés on 2/8/19.
+//  Copyright © 2019 FARC. All rights reserved.
+//
+
+import UIKit
+
+class KBaseModel: NSObject {
+    
+}
