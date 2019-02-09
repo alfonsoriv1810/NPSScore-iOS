656131391691a45b26277c1588540656838256fb
Alfonso Rivera Cortes <alfonso.riv1810@gmail.com>
2019-02-08 23:02:29 -0600

    8-Feb-2019 Se agrega proyecto
    -Se agrega el proyecto de NPS Score por parte de Kinedu para mí

---
+++ NPSScore/NPSScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NPSScore
+//
+//  Created by Alfonso Rivera Cortés on 2/8/19.
+//  Copyright © 2019 FARC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NPSScore/NPSScore/ViewControllers/KBaseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  KBaseViewController.swift
+//  NPSScore
+//
+//  Created by Alfonso Rivera Cortés on 2/8/19.
+//  Copyright © 2019 FARC. All rights reserved.
+//
+
+import UIKit
+
+class KBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
